3d434f00b5270f016d93feee0758bfa59d862a81
Manuel Guarniz <cruzemg95@gmail.com>
2019-03-24 10:53:04 -0500

    se creo una promesa de ejemplo

--- src/app/maestras/promesa/promesa.component.ts
+++ src/app/maestras/promesa/promesa.component.ts
@@ -7,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromesaComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.fnContador()
+      .then((req) => {
+        console.log(req);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log('Promesa terminada');
+      });
+  }
+
+  fnContador(): Promise<string> {
+    const promesa: Promise<string> = new Promise( (resolve, reject) => {
+      let contador = 0;
+      const interval = setInterval(() => {
+        contador += 1;
+        console.log(contador);
+        if (contador === 4) {
+          clearInterval(interval);
+          // resolve('termino');
+          reject('Algo salio mal');
+        }
+      }, 1000);
+    });
+    return promesa;
+  }
 
   ngOnInit() {
   }
